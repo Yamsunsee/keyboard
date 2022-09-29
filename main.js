e6a7f15cb16bfa37280aeba2042a621ea7465969
Yamsunsee <yamsunsee@gmail.com>
2022-09-29 13:18:09 +0700

    [Update] Random Sample Text And Add Backspace

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const keyboard = [
   ["q", "w", "d", "f", "g", "; :", "u", "k", "y", "p", "[ {", "] }"],
   ["a", "s", "e", "r", "l", "h", "n", "o", "i", "t", "/ ?"],
   ["z", "x", "c", "v", "' \"", "b", "j", "m", ", <", ". >"],
-  ["space"],
+  ["space", "backspace"],
 ];
 
 const keymap = {
@@ -40,8 +40,23 @@ const keymap = {
   ".": ",",
   "/": ".",
   " ": "space",
-  Backspace: "delete",
+  Backspace: "backspace",
 };
+
+const sampleTexts = [
+  "the quick brown fox jumped over the lazy dogs",
+  "two driven jocks help fax my big quiz",
+  "pack my box with five dozen liquor jugs",
+  "the five boxing wizards jump quickly",
+  "john quickly extemporized five two bags",
+  "viewing quizjzical abstracts mixed up hefty jocks",
+  "my girl wove six dozen plaid jackets before she quit",
+  "jackdaws love my big sphinx of quartz",
+  "brown jars prevented the mixture from freezing too quickly",
+  "farmer jack realized that big yellow quilts were expensive",
+];
+
+const randomeSampleText = () => sampleTexts[Math.floor(Math.random() * sampleTexts.length)];
 
 const screenElement = document.querySelector(".text-area");
 const resultElement = document.querySelector(".result");
@@ -49,7 +64,7 @@ const accElement = document.querySelector(".acc");
 const wpmElement = document.querySelector(".wpm");
 const resetButton = document.querySelector(".icon");
 const keyboardElement = document.querySelector(".keyboard");
-const sampleText = "the quick brown fox jumped over the lazy dogs";
+let sampleText = randomeSampleText();
 let validatedArray = new Array(sampleText.length).fill(0);
 let cursorPosition = 0;
 let isCompleted = false;
@@ -66,7 +81,7 @@ const showResult = () => {
 
 const validate = (key) => {
   if (isCompleted) return;
-  if (key === "delete") {
+  if (key === "backspace") {
     if (cursorPosition > 0) {
       wrongCount++;
       cursorPosition -= 1;
@@ -117,6 +132,7 @@ const render = () => {
 
 const reset = () => {
   resultElement.style.opacity = 0;
+  sampleText = randomeSampleText();
   validatedArray = new Array(sampleText.length).fill(0);
   cursorPosition = 0;
   isCompleted = false;
@@ -143,18 +159,25 @@ keyboard.forEach((row, index) => {
   keyboardElement.appendChild(rowElement);
 });
 
+keyboard.forEach((row) => {
+  row.forEach((key) => {
+    const keyElement = document.querySelector(`.key[data-key="${key.split(" ")[0]}"]`);
+    keyElement.onclick = (event) => {
+      validate(event.target.getAttribute("data-key"));
+    };
+  });
+});
+
 document.onkeydown = ({ key }) => {
   if (Object.keys(keymap).includes(key)) {
-    if (key !== "Backspace") {
-      const targetKey = document.querySelector(`.key[data-key="${keymap[key]}"]`);
-      targetKey.classList.add("press");
-    }
+    const targetKey = document.querySelector(`.key[data-key="${keymap[key]}"]`);
+    targetKey.classList.add("press");
     validate(keymap[key]);
   }
 };
 
 document.onkeyup = ({ key }) => {
-  if (Object.keys(keymap).includes(key) && key !== "Backspace") {
+  if (Object.keys(keymap).includes(key)) {
     const targetKey = document.querySelector(`.key[data-key="${keymap[key]}"]`);
     targetKey.classList.remove("press");
   } else if (key === "Enter" && isCompleted) {
